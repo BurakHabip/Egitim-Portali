b119401cc4888950f08e947389819feef43ce6d3
Burak Habip Delioğlu <Burak Habip Deliolu@BURAK>
2024-11-28 18:09:30 +0300

    Add project files.

---
+++ Egitim-Portali/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Egitim_Portali.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
